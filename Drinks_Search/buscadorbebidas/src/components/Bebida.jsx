839c34144c86f0746949456dc56b8a897d1a5b34
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2024-01-06 19:39:16 -0500

    Se muestra la información de las preparaciones a tráves de un card.

--- Drinks_Search/buscadorbebidas/src/components/Bebida.jsx
+++ Drinks_Search/buscadorbebidas/src/components/Bebida.jsx
@@ -1,16 +1,36 @@
-import React from 'react';
+import { Col, Card, Button } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 
 const Bebida = ({ bebida }) => {
     return (
-        <div>
-            {bebida.strDrink}
-        </div>
+        <Col md={6} lg={3}>
+            <Card className='mb-4'>
+                <Card.Img
+                    variant='top'
+                    src={bebida.strDrinkThumb}
+                    alt={`Imgen de ${bebida.strDrinkThumb}`}
+                />
+
+                <Card.Body>
+                    <Card.Title>
+                        {bebida.strDrink}
+                    </Card.Title>
+
+                    <Button
+                        variant='danger'
+                        className='w-100 text-uppercase mt-2'
+                    >
+                        Ver Receta
+                    </Button>
+                </Card.Body>
+            </Card>
+        </Col>
     );
 };
 
 Bebida.propTypes = {
     bebida: PropTypes.shape({
+        strDrinkThumb: PropTypes.string.isRequired,
         strDrink: PropTypes.string.isRequired,
     }).isRequired,
 };
