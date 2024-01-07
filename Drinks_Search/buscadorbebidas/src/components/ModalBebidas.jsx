079ace0d0317298e8313d2b34cc981dd541e9071
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2024-01-06 21:21:50 -0500

    Se agrega ventana modal que se mostrará al dar click en ver receta en la bebida.

--- Drinks_Search/buscadorbebidas/src/components/ModalBebidas.jsx
+++ Drinks_Search/buscadorbebidas/src/components/ModalBebidas.jsx
@@ -3,7 +3,22 @@ import useBebidas from "../hooks/useBebidas";
 
 const ModalBebidas = () => {
 
-    const { modal, handleModalClick } = useBebidas();
+    const { modal, handleModalClick, receta } = useBebidas();
+
+    const mostrarIngredientes = () => {
+        let ingredientes = [];
+
+        for(let i = 1; i < 16; i++){
+            if(receta[`strIngredient${i}`]){
+                /// .push modifica la variable original, en un useState esta opcion no debe aplicarse
+                ingredientes.push(
+                    <li>{receta[`strIngredient${i}`]} {receta[`strMeasure${i}`]}</li>
+                )
+            }
+        }
+
+        return ingredientes;
+    }
 
 
     return (
@@ -11,9 +26,25 @@ const ModalBebidas = () => {
             show={modal}
             onHide={handleModalClick}
         >
+            <Image
+                src={receta.strDrinkThumb}
+                alt={`Imágen receta ${receta.strDrink}`}
+            />
+
+            <Modal.Header>
+                <Modal.Title>
+                    {receta.strDrink}
+                </Modal.Title>
+            </Modal.Header>
             <Modal.Body>
-                Cuerpo de Modal
+                <div className="p-3">
+                    <h2>Instrucciones</h2>
+                    {receta.strInstructions}
+                    <h2>Ingredientes y Cantidades</h2>
+                    {mostrarIngredientes()}
+                </div>
             </Modal.Body>
+
         </Modal>
     )
 }
